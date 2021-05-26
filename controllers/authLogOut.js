05f343694321dad6237fea556b128674d52f65e6
EasyGoingLove <50263561+EasyGoingLove@users.noreply.github.com>
2021-05-26 05:12:27 +0300

    Delete controllers directory

--- controllers/authLogOut.js
+++
@@ -1,9 +0,0 @@
-const logOut = async(req,res) => {
-    res.cookie('jwt','logout',{
-        expires: new Date(Date.now() + 2*1000),
-        httpOnly: true
-    });
-    res.status(200).redirect('/');
-}
-
-export default logOut;
